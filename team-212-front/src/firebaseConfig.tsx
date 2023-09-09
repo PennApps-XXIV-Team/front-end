b554f55c10b97861c2d679946850566b23c3ced9
alexw <alex.j.witkowski@gmail.com>
2023-09-09 15:26:42 -0400

    added environment variables

---
+++ team-212-front/src/firebaseConfig.tsx
@@ -0,0 +1,17 @@
+import 'firebase/auth';
+import firebase from 'firebase/compat/app';
+
+export const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGE_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
+};
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export default firebaseApp;
